849adf0512c62629fac3ff4aeb391ad0cf17d9c3
xTidewaterx <79268288+xTidewaterx@users.noreply.github.com>
2025-10-17 00:16:59 +0200

    Refactor user registration API route

--- src/app/api/registerUser/route.js
+++ src/app/api/registerUser/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import admin from "firebase-admin";
 
-import serviceAccount from "../../../firebase/serviceAccountKey.json"; // adjust path
+
 import { v4 as uuidv4 } from "uuid";
 
 if (!admin.apps.length) {
@@ -74,3 +74,4 @@ export async function POST(req) {
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
+
